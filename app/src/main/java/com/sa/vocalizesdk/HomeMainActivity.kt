11e996a735a0c14d462680a6122fe5a1402f6a70
govindprajapati <govind.prajapati@solutionanalysts.com>
2019-11-12 18:51:05 +0530

    package name changes

---
+++ app/src/main/java/com/sa/vocalizesdk/HomeMainActivity.kt
@@ -0,0 +1,45 @@
+package com.sa.vocalizesdk
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sdk.vocalize.Vocalize
+import com.sdk.vocalize.VocalizeListener
+import kotlinx.android.synthetic.main.activity_main_home.*
+
+class HomeMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_home)
+
+        Vocalize.initialize(this@HomeMainActivity)
+
+        // Get the action and data from the intent to handle it.
+        val action: String? = intent?.action
+        when (action) {
+            // When the action is triggered by a deep-link, Intent.Action_VIEW will be used
+            Intent.ACTION_VIEW -> callMethod(intent)
+
+            // Otherwise start the app as you would normally do.
+            else -> {
+                tv_type?.let {
+                    it.text = "Nothing received"
+                }
+            }
+        }
+    }
+
+    private fun callMethod(intent: Intent?) {
+        Vocalize.searchKeyword(intent?.data, object : VocalizeListener {
+            override fun onError(error: String) {
+
+            }
+
+            override fun onSuccess() {
+
+            }
+
+        })
+    }
+}
